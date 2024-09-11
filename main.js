d1bbfd3d9aea85819c1e7af6592349d616bf7624
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-09-11 10:22:02 -0400

    Update main.js
    Fix accidental paste

--- main.js
+++ main.js
@@ -19,17 +19,3 @@ if (answer){
 } else {
   ti.output("idk what to tell you, man. It broke")
 }
-
-/**
- Oops - was this your work also ?
- **/
-/*
-let ti = new TextInterface(app, "Which soccer player are you?");
-ti.output("Hello and welcomeeee to Which soccer play are you.");
-ti.output("My name is Bob I'll be your host I hope your ready");
-let userName = await ti.readText();
-ti.output("It is lovely to meet you, " + userName);
-ti.output("whats your favorite number?");
-await ti.readText();
-
-*/
