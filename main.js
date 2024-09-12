a0131afb5af00f70511c37f1887c492fb1a62e4b
Andrecoder1234 <rocketpirate33@gmail.com>
2024-09-12 13:26:29 +0000

    addario

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ import { TextInterface } from "text-interface";
 let app = document.querySelector("#app");
 let ti = new TextInterface(app, "Which soccer player are you?");
 ti.output("Hello and welcomeeee to Which soccer play are you.");
-ti.output("My name is Bob I'll be your host I hope your ready");
+ti.output("My name is Bob I'll be your host what is your name?");
 let userName = await ti.readText();
 ti.output("It is lovely to meet you, " + userName);
-ti.output("whats your favorite number?");
+ti.output("Now lets start!");
+ti.output('do you like South America or Europe')
 await ti.readText();
+if (SouthAmerica) {
+  ti.output("Great option");
+  
+}
+//Ask hinkle what to do for "great option"
